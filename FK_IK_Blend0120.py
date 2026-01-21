7ca6c7223a319d3a7f83caf556802c605bee7cc6
Rikuto Tanaka <rikuto.12345st@gmail.com>
2026-01-21 09:13:14 +0900

    Create FK_IK_Blend0120.py
    作成中。
    選択ジョイントの最下層の子のジョイントのみがFK,IK,MIDの3つが複製されているが、それ以外はFK分のみ。また親子階層がずれている。

---
+++ FK_IK_Blend0120.py
@@ -0,0 +1,93 @@
+import maya.api.OpenMaya as om
+import maya.cmds as cmds
+
+# API2.0 を有効にする
+def maya_useNewAPI():
+    pass
+
+#class FK_IK_BlendRigCreate(om.MPxCommand):
+#selection = om.MGlobal.getActiveSelectionList()
+
+
+def getSelectJoint():
+    isSelection = cmds.ls(selection=True, type='joint')
+
+    if len(isSelection) == 1:
+        startJoint = isSelection[0]
+        endJoint = startJoint
+    
+    elif len(isSelection) == 2:
+        startJoint = isSelection[0]
+        endJoint = isSelection[1]
+
+    else:
+        cmds.error("ジョイントが選択されていないか、3つ以上選択されています")
+        return None
+    return startJoint,endJoint
+        
+def createChainList():
+    
+    start,end = getSelectJoint()
+    if start == None or end == None:
+        return
+    
+    current = start
+    chain = []
+    
+    #ジョイントを親から子の順でリストに格納
+    while True:
+
+        chain.append(current)
+
+        if current == end:
+            break
+        
+        child = cmds.listRelatives(current, children= True, type = 'joint')
+
+        if child is None or len(child) != 1:
+            cmds.error("子ジョイントが分岐しています")
+            return
+
+        current = child[0]
+
+    return chain
+
+def duplicateJoint(pulsName):
+    newChainList = []
+    chainList = createChainList()
+    for i in range(len(chainList)):
+        copyJnt = cmds.duplicate(chainList[i], po=True)[0]
+
+        newName = cmds.rename(copyJnt, chainList[i] + "_" + pulsName)
+
+        if len(newChainList) > 0:
+            cmds.parent(newName, newChainList[i - 1], relative=True)
+
+        newChainList.append(newName)
+
+    return newChainList
+
+def createJoint():
+    plusNameList = ["FK","IK","Mid"]
+
+    for i,plusNames in enumerate(plusNameList):
+
+        if i == 0:
+            jointFK = duplicateJoint(plusNames)
+
+        elif i == 1:
+            jointIK = duplicateJoint(plusNames)
+        
+        elif i == 2:
+            jointMID = duplicateJoint(plusNames)
+        
+        else:
+            break
+    
+    return jointFK,jointIK,jointMID
+
+
+
+createJoint()
+
+    
