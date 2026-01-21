88d0ce140c57c95df9d0a9b7a1c504f579e2d8de
Rikuto Tanaka <rikuto.12345st@gmail.com>
2026-01-21 16:25:28 +0900

    Fix and Update FK_IK_BlendCreate.py
    createJoint()で複製ジョイントを生成するたびに毎回
    duplicateJoint()でchainListを使うためにcreateChainList()を呼び出す構造となっていたため、何度も更新されるchainListの中身が二回目以降の更新で親子階層最下となるジョイントのみとなっていた。
    今回は全体をclassの中に収めて、self.chainListとして代入する形にして修正した。

--- FK_IK_BlendCreate.py
+++ FK_IK_BlendCreate.py
@@ -5,89 +5,100 @@ import maya.cmds as cmds
 def maya_useNewAPI():
     pass
 
-#class FK_IK_BlendRigCreate(om.MPxCommand):
 #selection = om.MGlobal.getActiveSelectionList()
 
+class FK_IK_BlendRigCreate(om.MPxCommand):
 
-def getSelectJoint():
-    isSelection = cmds.ls(selection=True, type='joint')
+    def __init__(self):
+        super().__init__()
+        self.chainList = []
+        self.plusNameList = ["FK","IK","Mid"]
 
-    if len(isSelection) == 1:
-        startJoint = isSelection[0]
-        endJoint = startJoint
-    
-    elif len(isSelection) == 2:
-        startJoint = isSelection[0]
-        endJoint = isSelection[1]
+    def getSelectJoint(self):
+        isSelection = cmds.ls(selection=True, type='joint')
 
-    else:
-        cmds.error("ジョイントが選択されていないか、3つ以上選択されています")
-        return None
-    return startJoint,endJoint
+        if len(isSelection) == 1:
+            startJoint = isSelection[0]
+            endJoint = startJoint
         
-def createChainList():
-    
-    start,end = getSelectJoint()
-    if start == None or end == None:
-        return
-    
-    current = start
-    chain = []
-    
-    #ジョイントを親から子の順でリストに格納
-    while True:
+        elif len(isSelection) == 2:
+            startJoint = isSelection[0]
+            endJoint = isSelection[1]
 
-        chain.append(current)
-
-        if current == end:
-            break
-        
-        child = cmds.listRelatives(current, children= True, type = 'joint')
-
-        if child is None or len(child) != 1:
-            cmds.error("子ジョイントが分岐しています")
-            return
-
-        current = child[0]
-
-    return chain
-
-def duplicateJoint(pulsName):
-    newChainList = []
-    chainList = createChainList()
-    for i in range(len(chainList)):
-        copyJnt = cmds.duplicate(chainList[i], po=True)[0]
-
-        newName = cmds.rename(copyJnt, chainList[i] + "_" + pulsName)
-
-        if len(newChainList) > 0:
-            cmds.parent(newName, newChainList[i - 1], relative=True)
-
-        newChainList.append(newName)
-
-    return newChainList
-
-def createJoint():
-    plusNameList = ["FK","IK","Mid"]
-
-    for i,plusNames in enumerate(plusNameList):
-
-        if i == 0:
-            jointFK = duplicateJoint(plusNames)
-
-        elif i == 1:
-            jointIK = duplicateJoint(plusNames)
-        
-        elif i == 2:
-            jointMID = duplicateJoint(plusNames)
-        
         else:
-            break
-    
-    return jointFK,jointIK,jointMID
+            cmds.error("ジョイントが選択されていないか、3つ以上選択されています")
+            return None
+        return startJoint,endJoint
+            
+    def createChainList(self):
+
+        start,end = self.getSelectJoint()
+        if start == None or end == None:
+            return
+        
+        current = start
+        chain = []
+        
+        #ジョイントを親から子の順でリストに格納
+        while True:
+
+            chain.append(current)
+
+            if current == end:
+                break
+            
+            child = cmds.listRelatives(current, children= True, type = 'joint')
+
+            if child is None or len(child) != 1:
+                cmds.error("子ジョイントが分岐しています")
+                return
+
+            current = child[0]
+            chainList = chain
+        
+        self.chainList = chain
+        print("指定ジョイント" , self.chainList)
+        return chain
+
+    def duplicateJoint(self,pulsName):
+
+        newChainList = []
+
+        for i in range(len(self.chainList)):
+            copyJnt = cmds.duplicate(self.chainList[i], po=True)[0]
+
+            newName = cmds.rename(copyJnt, self.chainList[i] + "_" + pulsName)
+
+            if len(newChainList) > 0:
+                cmds.parent(newName, newChainList[i - 1], relative=True)
+
+            newChainList.append(newName)
+
+        print("新しいジョイントリスト" , newChainList)
+        return newChainList
+
+    def doIt(self ,args):        
+
+        self.createChainList()
+
+        for i,plusNames in enumerate(self.plusNameList):
+
+            if i == 0:
+                jointFK = self.duplicateJoint(plusNames)
+
+            elif i == 1:
+                jointIK = self.duplicateJoint(plusNames)
+            
+            elif i == 2:
+                jointMID = self.duplicateJoint(plusNames)
+            
+            else:
+                break
+        
+        print()
+        return jointFK,jointIK,jointMID
+
+FK_IK_BlendRigCreate().doIt(None)
 
 
 
-createJoint()
-
-    
